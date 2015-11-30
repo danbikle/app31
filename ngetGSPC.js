333025605d1ff4a9d57b7422881231b3f980bf90
Dan Bikle <dan.bikle@gmail.com>
2015-11-30 17:13:13 +0000

    hello

--- ngetGSPC.js
+++ ngetGSPC.js
@@ -11,7 +11,7 @@ http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC
 var options = {
   host: 'ichart.finance.yahoo.com',
   // path: '/table.csv?s=%5EGSPC'
-  path: '/table.csv?s=FB'
+  path: '/table.csv?s=SQ'
 };
 
 callback = function(response) {
@@ -19,9 +19,16 @@ callback = function(response) {
   /* Hover debugger over response.
      I should see inside it. */
   response;
-  
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
   response.on('end', function () {
     console.log('response now on end.');
+    console.log(str);
   });
 
 }
