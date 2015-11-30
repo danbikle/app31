8bedb8f9bf8882d0b43e1ca81ff07c5d84a5514b
Dan Bikle <dan.bikle@gmail.com>
2015-11-30 16:58:35 +0000

    hello

--- ngetGSPC.js
+++ ngetGSPC.js
@@ -4,4 +4,14 @@ var http = require('http');
 
 http;
 
+/*
+I should GET this URL:
+http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC
+*/
+var options = {
+  host: 'ichart.finance.yahoo.com',
+  // path: '/table.csv?s=%5EGSPC'
+  path: '/table.csv?s=FB'
+};
+
 console.log('ngetGSPC.js done for now.')
