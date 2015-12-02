6482a95a872ce4ef1c8545ded3592f99ef9194c0
Dan Bikle <dan.bikle@gmail.com>
2015-12-02 23:59:46 +0000

    more

--- demo13.js
+++ demo13.js
@@ -168,7 +168,7 @@ function cb2(error, csv_a){
   // I should see results_o:
   results_o
   // I should show the JSON I use to build mymn:
-  d3.select('#json1').html(JSON.stringify(magicNet11json))
+  d3.select('#json1').html('var magicNet11json = '+JSON.stringify(magicNet11json))
   'end cb2'
 }
 
