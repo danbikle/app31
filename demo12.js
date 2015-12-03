4ee4e75131ed9f16e6a270f61b33a7e914dbaef5
Dan Bikle <dan.bikle@gmail.com>
2015-12-03 02:58:10 +0000

    more

--- demo12.js
+++ demo12.js
@@ -1,27 +1,41 @@
 /*
 demo12.js
-This is a simple demo.
+This is a simple demo,
+but it takes a L O O O N G time to run.
 */
 
-// This function should convert array into object full of features:
-var cp2ftr = function(cp_a) {
-  var features_o     = {}
-  features_o.pctlag1 = pctlagn(1,cp_a)
-  features_o.pctlag2 = pctlagn(2,cp_a)
-  features_o.pctlag4 = pctlagn(4,cp_a)
-  features_o.pctlag8 = pctlagn(8,cp_a)
-  return features_o
-}
+// I should get prices I want to learn from:
+d3.csv("/csv/GSPC.csv", cb1)
 
-// This function should convert array into array of labels:
-var cp2label   = function(bndry,cp_a) {
-  var pctlead  = pctlead1(cp_a)
-  var labels_a = pctlead.map(function(x){if (x<bndry) return 0; else return 1})
-  return labels_a
+// I should create a callback for d3.csv():
+function cb1(err, csv_a) {
+  if (err) throw err
+  // Yahoo gives the data by date descending.
+  // I should order it    by date ascending.
+  csv_a.reverse()
+  var cp_a = csv_a.map(function(row){return +row['Close']})
+  // I should define boundries of out-of-sample, train data
+  var train_end   = csv_a.length - 253  // 1 yr ago
+  var train_size  = 252*20              // 20 yrs
+  var train_start = train_end - train_size
+  // I should ensure train data and out-of-sample data do not mix:
+  var oos_start = train_end +    1
+  var oos_end   = csv_a.length - 1
+  var oos_size  = oos_end - oos_start
+  var pctlead = pctlead1(cp_a)
+  var pctlead_train = pctlead.slice(train_start,train_end)
+  // Now that I know pctlead_train, I can calculate train_median
+  var train_median = d3.median(pctlead_train)
+  var features_o   = cp2ftr(cp_a)
+  var labels_a     = cp2label(train_median,cp_a)
+  var train_o      = cr_train_o(train_start,train_end,features_o,labels_a);
+  // I should use train_o to create and train a new magicNet
+  cr_mn(train_o)
+  'cb1 done'
 }
 
 // This function should create training data from features, labels:
-var cr_train_o = function(train_start,train_end,features_o,labels_a) {
+function cr_train_o(train_start,train_end,features_o,labels_a) {
   var train_o = {}
   // I should get inside features_o and slice each feature
   for (ky in features_o) {
@@ -33,7 +47,7 @@ var cr_train_o = function(train_start,train_end,features_o,labels_a) {
 }
 
 // This function should use train_o to create and train a new magicNet.
-var cr_mn = function(train_o) {
+function cr_mn(train_o) {
   // The MagicNet class performs fully-automatic prediction on your data.
   // options struct:
   var opts = {} 
@@ -86,33 +100,5 @@ var cr_mn = function(train_o) {
     putjson_here.html(mnjson_s)
   }
 }
-// I should create a callback for d3.csv():
-var cb1 = function(err, csv_a) {
-  if (err) throw err
-  // Yahoo gives the data by date descending.
-  // I should order it    by date ascending.
-  csv_a.reverse()
-  var cp_a = csv_a.map(function(row){return +row['Close']})
-  // I should define boundries of out-of-sample, train data
-  var train_end   = csv_a.length - 253  // 1 yr ago
-  var train_size  = 252*20              // 20 yrs
-  var train_start = train_end - train_size
-  // I should ensure train data and out-of-sample data do not mix:
-  var oos_start = train_end +    1
-  var oos_end   = csv_a.length - 1
-  var oos_size  = oos_end - oos_start
-  var pctlead = pctlead1(cp_a)
-  var pctlead_train = pctlead.slice(train_start,train_end)
-  // Now that I know pctlead_train, I can calculate train_median
-  var train_median = d3.median(pctlead_train)
-  var features_o   = cp2ftr(cp_a)
-  var labels_a     = cp2label(train_median,cp_a)
-  var train_o      = cr_train_o(train_start,train_end,features_o,labels_a);
-  // I should use train_o to create and train a new magicNet
-  cr_mn(train_o)
-  'cb1 done'
-}
 
-d3.csv("/csv/GSPC.csv", cb1)
-
-'bye'
+'end'
